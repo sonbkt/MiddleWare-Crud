4a6e3575fee02a0d57aeeba96b8ad7918890e1c3
Achiliku <epimetheus.hp94@gmail.com>
2017-02-21 16:47:05 +0700

    add users

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-const express = require('express');
-const app     = express();
-const router  = require('./router');
-
-
-router.get('/', (req, res) => {
-  res.send("Now you see me");
+const express         = require('express');
+const app             = express();
+const expressNunjucks = require('express-nunjucks');
+const router          = require('./router');
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/test';
+const bodyParser= require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('views', __dirname + '/templates');
+const njk = expressNunjucks(app);
+let db
+MongoClient.connect(url, (err, database) => {
+  db = database;
+  console.log('You just acess mongodb');
 })
-
+router.get('/users/add', (req, res) => {
+  res.render('users/user-add.njk.html');
+});
+router.post('/users', (req, res) => {
+  db.collection('users').save(req.body, (err, result) => {
+  })
+})
 app.use(router);
 app.listen(3000);
