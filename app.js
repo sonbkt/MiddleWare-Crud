4fd5790e75160d6ea6dd4db15cd42e26de6a0d84
Achiliku <epimetheus.hp94@gmail.com>
2017-02-22 14:29:22 +0700

    get user detail

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ const express         = require('express');
 const app             = express();
 const expressNunjucks = require('express-nunjucks');
 const router          = require('./router');
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient     = require('mongodb').MongoClient;
+const ObjectId        = require('mongodb').ObjectId;
 const url = 'mongodb://localhost:27017/test';
 const bodyParser= require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('views', __dirname + '/templates');
 const njk = expressNunjucks(app);
-let db
+let db;
+// MongoClient.connect(url, (err, database) => {
+//   db = database;
+//   console.log('You just acess mongodb');
+// })
 MongoClient.connect(url, (err, database) => {
   db = database;
-  console.log('You just acess mongodb');
 })
 router.get('/users/add', (req, res) => {
   res.render('users/user-add.njk.html');
@@ -20,5 +24,18 @@ router.post('/users', (req, res) => {
   db.collection('users').save(req.body, (err, result) => {
   })
 })
+// Get user detail
+router.get('/users/:id', (req, res) => {
+  db.collection('users').findOne({
+    _id : new ObjectId(req.params.id)
+  }).then((user) => {
+      console.log(user);
+      res.render('users/user-detail.njk.html', {user: user});
+  })
+})
+
+
+
+
 app.use(router);
 app.listen(3000);
