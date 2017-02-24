6bfa21c1047d96ef296b7f8cb20d8d3a802c25e3
Achiliku <epimetheus.hp94@gmail.com>
2017-02-24 18:41:40 +0700

    add users

--- router.js
+++ router.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router  = express.Router();
 const usersController = require('./users.controller');
+const userNameNotExitedMiddleware = require('./username-notexisted.middleware.js');
 
 router.get('/users/add', usersController.addForm);
-router.post('/users/add', usersController.add);
+router.post('/users/add', userNameNotExitedMiddleware, usersController.add);
+
 
 module.exports = router;
