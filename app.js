1462aa193857cbba1dfc8839e068408d33a9601d
Achiliku <epimetheus.hp94@gmail.com>
2017-02-24 14:17:47 +0700

    full CRUD without Middleware

--- app.js
+++ app.js
@@ -1,52 +1,73 @@
-const express         = require('express');
-const app             = express();
-const expressNunjucks = require('express-nunjucks');
-const router          = require('./router');
-const MongoClient     = require('mongodb').MongoClient;
-const ObjectId        = require('mongodb').ObjectId;
+const express = require('express');
+const app     = express();
+const router  = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId    = require('mongodb').ObjectId;
 const url = 'mongodb://localhost:27017/test';
-const bodyParser= require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
+const expressNunjucks = require('express-nunjucks');
 app.set('views', __dirname + '/templates');
 const njk = expressNunjucks(app);
-let db;
-// MongoClient.connect(url, (err, database) => {
-//   db = database;
-//   console.log('You just acess mongodb');
-// })
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
 MongoClient.connect(url, (err, database) => {
   db = database;
 })
+
+// add users form
 router.get('/users/add', (req, res) => {
   res.render('users/user-add.njk.html');
 });
-router.post('/users', (req, res) => {
+// Add User
+router.post('/users/add', (req, res) => {
   db.collection('users').save(req.body, (err, result) => {
+    res.redirect('/users');
+    console.log("Save to database");
   })
 })
-// Get user detail
+// List User
+router.get('/users', (req, res) => {
+  db.collection('users').find().toArray().then((allusers) => {
+       res.render('users/user-list.njk.html', {users : allusers});
+  })
+})
+//User Detail
 router.get('/users/:id', (req, res) => {
   db.collection('users').findOne({
     _id : new ObjectId(req.params.id)
   }).then((user) => {
-      console.log(user);
-      res.render('users/user-detail.njk.html', {user: user});
+    res.render('users/user-detail.njk.html', {user : user});
   })
 })
-// Delete user
+// Delete Users
 router.get('/users/delete/:id', (req, res) => {
   db.collection('users').deleteOne({
     _id : new ObjectId(req.params.id)
-  }, (err, result) => {
-    console.log(result);
-    res.redirect('/users/add');
+  }).then(() => {
+    res.redirect('/users');
   })
 })
 
-//
+// Update Users Form
+router.get('/users/update/:id', (req, res) => {
+  db.collection('users').findOne({
+    _id : new ObjectId(req.params.id)
+  }).then((user) => {
+      res.render('users/user-update.njk.html', {user : user});
+  })
+})
 
-
-
-
+// Update
+router.post('/users/update/:id', (req, res) => {
+  db.collection('users')
+  .updateOne({
+    _id : new ObjectId(req.params.id)
+  }, {
+    $set : {name : req.body.name},
+    $set : {age  : req.body.age}
+  }).then(() => {
+    res.redirect('/users');
+  })
+})
 app.use(router);
 app.listen(3000);
